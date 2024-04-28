324f4357919cc2737f7c74afc9d33280bddd711a
AzerX2 <bpt.cor@gmail.com>
2024-04-28 16:35:49 +0200

    Ajout mention légale et changement mineure

--- models/inscriptionModel.js
+++ models/inscriptionModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const inscriptionSchema = new mongoose.Schema({
     atelier: String,
     nom: String,
-    prenom: String
+    prenom: String,
+    mail: String
 });
 
 module.exports = mongoose.model('Inscription', inscriptionSchema);
